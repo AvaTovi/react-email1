0cda852a6de5d7575875ed66369a5cae7e5ee751
Ava Tovi <avatovi45@icloud.com>
2025-01-22 20:14:25 -0800

    finished the button

--- emails/FindHousing.tsx
+++ emails/FindHousing.tsx
@@ -11,7 +11,6 @@ export const FindHousingEmail = () => {
       </Head>
       <Body>
         <div style={{ width: "736px", height: "2856px", top: "3391px", left: "6336px", borderRadius: "24px" }}>
-<<<<<<< HEAD
           {/* Parent Frame: Main Container */}
 
           {/* Header Section */}
@@ -29,33 +28,18 @@ export const FindHousingEmail = () => {
             <div style={{ width: "510.63px", height: "132px", top: "183px", left: "113px", fontFamily: "Roboto, sans-serif", fontWeight: 700, fontSize: "48.63px", lineHeight: "66px", textAlign: "center", position: "absolute", color: "#FFFFFF" }}>FIND HOUSING NEAR YOUR CAMPUS</div>
 
             {/* Text: Description */}
-=======
-          <div style={{ width: "736px", height: "38px", borderTopLeftRadius: "24px", borderTopRightRadius: "24px", backgroundColor: "black", display: "flex", alignItems: "center", justifyContent: "center", border: "2px solid black", boxSizing: "border-box" }}>
-            <h1 style={{ width: "134px", height: "24px", borderTop: "7px", borderLeft: "301px", transform: "rotate(0deg)", color: "#FFFFFF", fontFamily: "Roboto, sans-serif", fontWeight: 600, fontSize: "14px", lineHeight: "24px", textAlign: "center", whiteSpace: "nowrap" }}>Reset Your Password</h1>
-          </div>
-
-          <div style={{ width: "736px", height: "640px", display: "flex", position: "relative", justifyContent: "center", alignItems: "center", flexDirection: "column", backgroundColor: "#FF5A60" }}>
-            <div style={{ width: "93px", height: "350px", top: "149px", left: "322px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 700, fontSize: "12px", lineHeight: "24px", letterSpacing: "2%", alignItems: "center", textAlign: "center", whiteSpace: "nowrap", color: "#FFFFFF" }}>ARE YOU READY</div>
-
-            <div style={{ width: "510.63px", height: "132px", top: "183px", left: "113px", fontFamily: "Roboto, sans-serif", fontWeight: 700, fontSize: "48.63px", lineHeight: "66px", textAlign: "center", position: "absolute", color: "#FFFFFF" }}>FIND HOUSING NEAR YOUR CAMPUS</div>
-
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
             <div style={{ width: "537px", height: "52px", top: "328px", left: "100px", fontFamily: "inter, sans-serif", fontWeight: 400, fontSize: "18px", lineHeight: "26px", textAlign: "center", position: "absolute", color: "#FCFCFC" }}>
               Introducing the ideal platform for college students to find and <br /> rent apartments near their campus without the hassle.
             </div>
           </div>
 
-<<<<<<< HEAD
           {/* Section: Discover What's Coming */}
-=======
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
           <div style={{ width: "620.07px", height: "66px", top: "726px", left: "58px", gap: "12.38px", display: "flex", alignItems: "center", justifyContent: "flex-start", position: "absolute" }}>
             <div style={{ width: "256.08px", height: "65px", transform: "rotate(0deg)", fontFamily: "Gilroy, sans-serif", fontWeight: 799, fontSize: "51.78px", lineHeight: "65.77px", textAlign: "center", color: "#515558" }}>DISCOVER</div>
             <div style={{ width: "120.12px", height: "54px", marginLeft: "20px", transform: "rotate(0deg)", fontFamily: "Roboto,sans-serif", fontWeight: 300, fontStyle: "italic", fontSize: "42.45px", lineHeight: "53.92px", textAlign: "center", fontStretch: "condensed", color: "#515558" }}>Whats</div>
             <div style={{ width: "219.12px", height: "65px", transform: "rotate(0deg)", fontFamily: "Gilroy, sans-serif", fontWeight: 799, fontSize: "51.78px", lineHeight: "65.77px", textAlign: "center", color: "#515558" }}>COMING</div>
           </div>
 
-<<<<<<< HEAD
           {/* Section: Description */}
           <div style={{ width: "554px", height: "84px", top: "816px", left: "91px", fontFamily: "Roboto, sans-serif", fontWeight: 300, fontSize: "18px", lineHeight: "28px", textAlign: "center", alignItems: "center", justifyContent: "center", display: "flex", position: "absolute", color: "#545558" }}>Good vibes spark good ideas. Mycollegeease through dozens of campus partners in North Carolina offers almost all students' needs to finding housing around campus.</div>
 
@@ -66,95 +50,57 @@ export const FindHousingEmail = () => {
             {/* Feature List */}
             <div style={{ width: "543px", height: "70px", gap: "42px", display: "flex" }}>
               {/* Feature 1 */}
-=======
-          <div style={{ width: "554px", height: "84px", top: "816px", left: "91px", fontFamily: "Roboto, sans-serif", fontWeight: 300, fontSize: "18px", lineHeight: "28px", textAlign: "center", alignItems: "center", justifyContent: "center", display: "flex", position: "absolute", color: "#545558" }}>Good vibes spark good ideas. Mycollegeease through dozens of campus partners in North Carolina offers almost all students' needs to finding housing around campus.</div>
-
-          <div style={{ width: "543px", height: "238px", top: "932px", left: "97px", gap: "32px", position: "absolute", justifyContent: "center" }}>
-            <div style={{ width: "543px", height: "24px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 700, fontSize: "18px", lineHeight: "24px", letterSpacing: "2%", alignItems: "center", justifyContent: "center", display: "flex", marginBottom: "20px", color: "#515558" }}>Our Features</div>
-
-            <div style={{ width: "543px", height: "70px", gap: "42px", display: "flex" }}>
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
               <div style={{ width: "250px", height: "70px", gap: "8px", display: "flex", flexDirection: "column" }}>
                 <div style={{ width: "250px", height: "28px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 800, fontSize: "18px", lineHeight: "28px", color: "#515558" }}>Lease Property</div>
                 <div style={{ width: "250px", height: "34px", fontFamily: "Roboto, sans-serif", fontWeight: 400, fontSize: "12px", lineHeight: "17.16px", letterSpacing: "0.1px", color: "#545558" }}>Lorem ipsum dolor sit amet, consectetur elit. Vivamus in velit interdum, facilisis lectus quis.</div>
               </div>
-<<<<<<< HEAD
               {/* Feature 2 */}
-=======
-
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
               <div style={{ width: "250px", height: "70px", gap: "8px", right: 0, display: "flex", flexDirection: "column" }}>
                 <div style={{ width: "250px", height: "28px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 800, fontSize: "18px", lineHeight: "28px", whiteSpace: "nowrap", color: "#515558" }}>Shared Room</div>
                 <div style={{ width: "250px", height: "34px", fontFamily: "Roboto, sans-serif", fontWeight: 400, fontSize: "12px", lineHeight: "17.16px", letterSpacing: "0.1px", bottom: 0, color: "#545558" }}>Lorem ipsum dolor sit amet, consectetur elit. Vivamus in velit interdum, facilisis lectus quis.</div>
               </div>
             </div>
 
-<<<<<<< HEAD
             {/* Additional Features */}
             <div style={{ width: "543px", height: "140px", flexDirection: "column", gap: "8px", display: "flex", position: "relative" }}>
               {/* Feature 3 */}
-=======
-            <div style={{ width: "543px", height: "140px", flexDirection: "column", gap: "8px", display: "flex", position: "relative" }}>
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
               <div style={{ width: "250px", height: "70px", gap: "8px", position: "absolute", bottom: 20, left: "0" }}>
                 <div style={{ width: "250px", height: "28px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 800, fontSize: "18px", lineHeight: "28px", color: "#515558" }}>Sublease Property</div>
                 <div style={{ width: "250px", height: "34px", fontFamily: "Roboto, sans-serif", fontWeight: 400, fontSize: "12px", lineHeight: "17.16px", letterSpacing: "0.1px", position: "absolute", bottom: 0, left: 0, color: "#545558" }}>Lorem ipsum dolor sit amet, consectetur elit. Vivamus in velit interdum, facilisis lectus quis.</div>
               </div>
 
-<<<<<<< HEAD
               {/* Feature 4 */}
               <div style={{ width: "250px", height: "68px", gap: "8px", bottom: 20, right: 0, position: "absolute" }}>
                 <div style={{ width: "250px", height: "26px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 800, fontSize: "16px", lineHeight: "26px", color: "#515558" }}>Move-in/Out Support</div>
-=======
-              <div style={{ width: "250px", height: "68px", gap: "8px", bottom: 20, right: 0, position: "absolute" }}>
-                <div style={{ width: "250px", height: "26px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 800, fontSize: "16 px", lineHeight: "26px", color: "#515558" }}>Move-in/Out Support</div>
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
                 <div style={{ width: "250px", height: "34px", fontFamily: "Roboto, sans-serif", fontWeight: 400, fontSize: "12px", lineHeight: "17.16px", letterSpacing: "0.1px", bottom: 0, right: 0, position: "absolute", color: "#545558" }}>Lorem ipsum dolor sit amet, consectetur elit. Vivamus in velit interdum, facilisis lectus quis.</div>
               </div>
             </div>
           </div>
 
-<<<<<<< HEAD
           {/* Subscription Section */}
           <div style={{ width: "343px", height: "130px", top: "1242px", left: "197px", gap: "24px", position: "absolute", alignItems: "center", textAlign: "center" }}>
             {/* Subscription Text */}
-=======
-          <div style={{ width: "343px", height: "130px", top: "1242px", left: "197px", gap: "24px", position: "absolute", alignItems: "center", textAlign: "center" }}>
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
             <div style={{ width: "343px", height: "54px", gap: "4px" }}>
               <div style={{ width: "343px", height: "26px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 700, fontSize: "16px", lineHeight: "26px", letterSpacing: "2%", alignItems: "center", justifyContent: "center", color: "#515558" }}>Be the first to know when new feature lands.</div>
               <div style={{ width: "343px", height: "24px", fontFamily: "Roboto, sans-serif", fontWeight: 400, fontSize: "14px", lineHeight: "24px", letterSpacing: "0.1px", alignItems: "center", justifyContent: "center", color: "#545558" }}>Enter your email address here</div>
             </div>
-<<<<<<< HEAD
             {/* Subscription Input and Button */}
-=======
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
             <div style={{ width: "310px", height: "38px", borderRadius: "36px", border: "1px solid #CBD5E1", padding: "8px 8px 8px 24px", gap: "16px", bottom: 0, position: "absolute", alignItems: "center", display: "flex", color: "#CBD5E1" }}>
               <div style={{ width: "195px", height: "22px", gap: "8px" }}>
                 <div style={{ width: "106px", height: "22px", fontFamily: "Inter, sans-serif", fontWeight: 400, fontSize: "14px", lineHeight: "22px", justifyContent: "center", alignItems: "center", color: "#94A3B8" }}>Input your email</div>
               </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
               <div style={{ fontVariant: "contained", backgroundColor: buttonColor, fontSize: "medium", width: "120px", height: "30px", borderRadius: "24px", padding: "8px", color: "white", display: "flex", justifyContent: "center", alignItems: "center", cursor: "pointer" }}>Subscribe</div>
             </div>
           </div>
 
-<<<<<<< HEAD
           {/* Explore Section */}
-=======
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
           <div style={{ width: "538.75px", height: "66px", top: "1433px", left: "99px", gap: "8px", display: "flex", alignItems: "center", justifyContent: "flex-start", position: "absolute" }}>
             <div style={{ width: "240px", height: "60px", transform: "rotate(0deg)", fontFamily: "Gilroy, sans-serif", fontWeight: 799.99991, fontSize: "49px", lineHeight: "65.77px", textAlign: "center", color: "#515558" }}>EXPLORE</div>
             <div style={{ width: "131px", height: "54px", transform: "rotate(0deg)", fontFamily: "Roboto, sans-serif", fontWeight: 0.0000000001, fontStyle: "italic", fontSize: "40.45px", lineHeight: "53.92px", margin: "0", textAlign: "center", fontStretch: "ultra-condensed", color: "#515558" }}>Special</div>
             <div style={{ width: "240px", height: "60px", transform: "rotate(0deg)", fontFamily: "Gilroy, sans-serif", fontWeight: 799.99991, fontSize: "49px", lineHeight: "65.77px", textAlign: "center", color: "#515558" }}>ORDER</div>
           </div>
 
-<<<<<<< HEAD
           {/* Image Gallery Section */}
-=======
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
           <div style={{ width: "610px", height: "307px", top: "1559px", left: "63px", borderTopLeftRadius: "10px", borderTopRightRadius: "10px", position: "absolute", display: "flex", border: "2px solid black" }}>
             <img />
           </div>
@@ -165,43 +111,37 @@ export const FindHousingEmail = () => {
             <img />
           </div>
 
-<<<<<<< HEAD
           {/* Button: Explore More */}
-=======
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
           <div style={{ width: "120px", height: "35px", top: "2190px", left: "285px", borderRadius: "100px", padding: "14px 24px", gap: "10px", display: "flex", position: "absolute", justifyContent: "center", alignItems: "center", textAlign: "center", cursor: "pointer", backgroundColor: buttonColor, border: "black" }}>
             <div style={{ width: "106px", height: "26px", fontFamily: "Poppins, sans-serif", fontWeight: 700, fontSize: "16px", lineHeight: "26px", color: "#FFFFFF" }}>Explore More</div>
           </div>
 
-<<<<<<< HEAD
-          {/* Contact Section */}
-=======
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
+          {/* parent fraame Section */}
           <div style={{ width: "716px", height: "327px", top: "2273px", left: "10px", position: "absolute", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#F5F5FB", border: "2px solid black" }}>
+            {/*Child Frame*/}
             <div style={{ width: "565px", height: "204px", top: "2335px", left: "86px", gap: "42px", display: "flex", justifyContent: "flex-start", alignItems: "center", flexDirection: "column", border: "2px solid black" }}>
+              {/*Text frame*/}
               <div style={{ width: "468px", height: "108px", position: "absolute", display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center", border: "2px solid black" }}>
                 <div style={{ width: "327px", height: "32px" }}>
-<<<<<<< HEAD
                   {/*Have Questions? Get In Contact section*/}
                   <div style={{ width: "327px", height: "32px", fontFamily: "YourFontFamily, sans-serif", fontWeight: 500, fontSize: "1.25rem", lineHeight: "32px", letterSpacing: "0.15px", textAlign: "center" }}>Have Questions? Get In Touch</div>
                 </div>
                 <div style={{ width: "468px", height: "52px", bottom: 0, position: "absolute" }}>
                   {/*Long text Section*/}
                   <div style={{ width: "468px", height: "52px", fontFamily: "YourFontFamily, sans-serif", fontWeight: "regular", fontSize: "16px", lineHeight: "26px", letterSpacing: "0.17px", textAlign: "center", color: "#94A3B8" }}>A great platform to find and lease your envisioned property without any agent and commissions</div>
-=======
-                  <div style={{ width: "327px", height: "32px", fontFamily: "YourFontFamily, sans-serif", fontWeight: 500, fontSize: "1.25rem", lineHeight: "32px", letterSpacing: "0.15px", textAlign: "center" }}>Have Questions? Get In Touch</div>
->>>>>>> 763137ed94b371187ad09a15a708b554330948de
                 </div>
-                <div style={{ width: "468px", height: "52px", bottom: 0, position: "absolute" }}>
-                  <div style={{ width: "468px", height: "52px", fontFamily: "YourFontFamily, sans-serif", fontWeight: "regular", fontSize: "16px", lineHeight: "26px", letterSpacing: "0.17px", textAlign: "center", color: "#94A3B8" }}>A great platform to find and lease your envisioned property without any agent and commisions</div>
-                </div>
-                <div style={{}}></div>
+              </div>
+              {/*contact us button frame*/}
+              <div style={{ width: "244px", height: "54px", bottom: 0, borderRadius: "100px", padding: "14px 24px", gap: "10px", display: 'flex', position: "absolute", alignItems: "center", justifyContent: "flex-bottom", backgroundColor: buttonColor, border: "2px solid black" }}>
+              <div style={{ }}>
+                
               </div>
             </div>
           </div>
         </div>
-      </Body>
-    </Html>
+      </div>
+    </Body>
+  </Html>
   );
 };
 export default FindHousingEmail;
