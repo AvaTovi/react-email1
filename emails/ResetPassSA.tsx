bf7c0fc4e0298df5e407ed083cc97e9a3f02156e
Ava Tovi <avatovi45@icloud.com>
2025-03-15 14:40:26 -0700

    FINISHED CODE

--- emails/ResetPassSA.tsx
+++ emails/ResetPassSA.tsx
@@ -24,11 +24,11 @@ export const ResetPassSA = () => {
               <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742063210/react-email-images/csdeibmo4yfwx1euclko.png" alt="big pic" />
             </div>
 
-            <Section className="mx-auto w-full max-w-[600px] relative flex justify-center items-center whitespace-normal">
+            <Section className="mx-auto w-full max-w-[600px] relative flex justify-center items-center pt-[60px] whitespace-normal">
               <Text className="w-full text-[35px] leading-[38px] text-center" style={{ fontFamily: "Poppins, sans-serif", fontWeight: 799 }}>
                 Reset Your Password
               </Text>
-              <Text className="w-full font-normal text-[20px] pt-[15px] leading-[30px] text-center text-[#4C4C4C]" style={{ fontFamily: "Poppins, sans-serif" }}>
+              <Text className="w-full font-normal text-[21px] pt-[15px] leading-[30px] text-center text-[#4C4C4C]" style={{ fontFamily: "Poppins, sans-serif" }}>
                 We received a request to reset the password for your account. <br />
                 Please click the button below to create a new password.
               </Text>
@@ -38,7 +38,7 @@ export const ResetPassSA = () => {
                 Reset My Password
               </Button>
             </Section>
-            <Section className="mx-auto w-full max-w-[560px] relative flex flex-col justify-start items-stretch whitespace-normal">
+            <Section className="mx-auto w-full max-w-[600px] relative flex flex-col justify-start items-stretch whitespace-normal">
               <Text className="w-full font-normal text-[20px] pt-[45px] leading-[22px] text-[#4C4C4C]" style={{ fontFamily: "Poppins, sans-serif" }}>
                 <span className="font-normal text-gray-600 block">or copy and paste this URL into your browser</span>
                 <span className="font-normal underline block leading-[40px] decoration-solid decoration-auto cursor-pointer text-blue-600">https://skateapp.net/request/reset_password/foo</span>
@@ -70,6 +70,30 @@ export const ResetPassSA = () => {
                 </span>
               </Text>
             </Section>
+            <Section className="pt-[50px] flex justify-center items-center">
+              <Text className="font-semibold text-[19px] text-center text-[#4A4A45]" style={{ fontFamily: "Poppins, sans-serif" }}>
+                Download SkateApp &
+              </Text>
+              <Text className="font-light text-[18px] leading-[15px] text-center text-[#4A4A45] " style={{ fontFamily: "Poppins, sans-serif" }}>
+                Find hidden gems wherever you shred.
+              </Text>
+            </Section>
+            <div className=" flex flex-row justify-center items-center ">
+              <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742073455/react-email-images/m8irf4aw1bfnmrlaavw3.png" alt="apple1" className="w-100 h-auto mr-[-9px] cursor-pointer"></Img>
+              <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742074247/react-email-images/zqssh04t7eiawzcuagix.png" alt="GooglePlay1" className="w-100 h-auto ml-[-9px] cursor-pointer"></Img>
+            </div>
+            <div style={{ width: "100%", maxWidth: "560px", position: "relative", margin: "25px auto 0 auto", borderTopWidth: "1px", borderTopStyle: "solid", borderColor: "rgba(21,19,19,0.21)", display: "block" }}></div>
+            <div className="w-full flex flex-col items-center justify-between pt-[30px] gap-x-6">
+              <Text className="font-light text-[15px] leading-[26px] text-[#4A4A45]" style={{ fontFamily: "Poppins, sans-serif" }}>
+                Spread the word!
+              </Text>
+              <div className="flex flex-row items-center gap-3">
+                <Img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook Logo" className="w-[20px] h-[22px] cursor-pointer" />
+                <Img src="https://cdn.prod.website-files.com/5d66bdc65e51a0d114d15891/64cebdd90aef8ef8c749e848_X-EverythingApp-Logo-Twitter.jpg" alt="X Logo" className="w-[20px] h-[22px] cursor-pointer" />
+                <Img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram Logo" className="w-[20px] h-[22px] border-2 cursor-pointer" />
+              </div>
+            </div>
+            <div className="pt-[40px]"></div>
           </Container>
         </Body>
       </Tailwind>
