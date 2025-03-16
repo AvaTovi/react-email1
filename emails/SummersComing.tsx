a9760f7e492a0f8dc954a554e50db0ce10456768
Ava Tovi <avatovi45@icloud.com>
2025-03-16 14:02:45 -0700

    Finished more

--- emails/SummersComing.tsx
+++ emails/SummersComing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Body, Container, Html, Img, Text, Head, Tailwind } from "@react-email/components";
+import { Body, Container, Section, Html, Img, Text, Head, Tailwind } from "@react-email/components";
 
 export const SummersComing = () => {
   return (
@@ -10,12 +10,18 @@ export const SummersComing = () => {
             <Container>
               <div className="max-w-[600px] flex justify-center relative">
                 <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742157020/react-email-images/owvvmd8xfgiax32ryplh.png" alt="Big frame" className="w-full h-full object-cover" />
-                <div className="absolute justify-center pt-[20px] items-center flex flex-row text-center">
+                <div className="absolute justify-center pt-[10px] items-center flex flex-row text-center">
                   <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742059963/react-email-images/bxve7zpqde3shxos0b0a.png" alt="icon1" className="mr-[5px] w-[25px]" />
                   <Text className="font-extrabold text-[16px] leading-[28px] text-[#FCFCFF]" style={{ fontFamily: "Gilroy, sans-serif" }}>
                     SkateApp
                   </Text>
                 </div>
+                <Section className="absolute flex flex-row justify-center pt-[50px]">
+                  <Text className="text-[36px] leading-[35px] pt-[130px] text-center text-[#FCFCFF]" style={{ fontFamily: "Gilroy, sans-serif", fontWeight: 750 }}>
+                    SUMMER'S COMING
+                  </Text>
+                  <div style={{ width: "100%", maxWidth: "100px", position: "relative", margin: "25px auto 0 auto", borderTopWidth: "1px", borderTopStyle: "solid", borderColor: "white", display: "block" }}></div>
+                </Section>
               </div>
             </Container>
           </Body>
