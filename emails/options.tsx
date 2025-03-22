d1a1df79f8d78744bb5d0e981b14f8eb791de553
Ava Tovi <avatovi45@icloud.com>
2025-03-22 11:38:15 -0700

    added some text

--- emails/options.tsx
+++ emails/options.tsx
@@ -8,40 +8,36 @@ export const options = () => {
         <Head />
         <Body>
           <Container className="max-w-[600px] mx-auto">
+            {/* Logo + Title */}
             <div className="w-full justify-center pt-[10px] items-center flex flex-row text-center">
-              <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742059963/react-email-images/bxve7zpqde3shxos0b0a.png" alt="icon1" className="mr-[5px] w-[30px]" />
-              <Text className="font-extrabold text-[17px] leading-[28px] text-[black]" style={{ fontFamily: "Gilroy, sans-serif" }}>
+              <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742059963/react-email-images/bxve7zpqde3shxos0b0a.png" alt="icon1" className="mr-[10px] w-[35px]" />
+              <Text className="font-extrabold text-[18px] leading-[28px] text-[black]" style={{ fontFamily: "Gilroy, sans-serif" }}>
                 SkateApp
               </Text>
             </div>
             <Section className="max-w-[500px] w-full flex pt-[30px] justify-center mx-auto">
-              <div style={{ width: "500px", height: "1px", backgroundColor: "gray" }} />
-              <div className="max-w-[500px] w-full flex flex-row justify-start items-center ">
-                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
-                  Skatepark
-                </Text>
-                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
-                  Street Spot
-                </Text>
-                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
-                  DIY
-                </Text>
-                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
-                  In progress
-                </Text>
-                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
-                  Dead Spot
-                </Text>
+              <div className="w-[500px] h-[1px] bg-gray-500 mx-auto" />
+              <div className="max-w-[500px] w-full flex flex-row justify-start items-center">
+                {["Skatepark", "Street Spot", "DIY", "In progress", "Dead Spot"].map((item, index) => (
+                  <Text key={index} className="font-light text-[14px] m-[20px] text-center text-[#2E2F30]" style={{ fontFamily: "Gilroy, sans-serif" }}>
+                    {item}
+                  </Text>
+                ))}
               </div>
-              <div style={{ width: "500px", height: "1px", backgroundColor: "gray" }} />
-              <div className="max-w-[500px] w-full relative ">
-                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664337/react-email-images/fyk6ktfzbk3efhovttz9.png" alt="Big frame1" className="w-full object-cover pt-[50px]" />
+              <div className="w-[500px] h-[1px] bg-gray-500 mx-auto" />
+              <div className="max-w-[500px] w-full relative flex justify-center pt-[50px]">
+                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664337/react-email-images/fyk6ktfzbk3efhovttz9.png" alt="Big frame1" className="w-full object-cover" />
+                <div className="absolute flex flex-row justify-center items-center">
+                  <Text className="font-light text-[24px] text-center text-[#FCFCFF]" style={{ fontFamily: "Gilroy, sans-serif" }}>
+                    CICHANO SKATEPARK
+                  </Text>
+                </div>
               </div>
-              <div className="max-w-[500px] w-full relative ">
-                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664503/react-email-images/czdm06rsl3vxkpgnpwtj.png" alt="Big frame2" className="w-full object-cover pt-[25px]" />
+              <div className="max-w-[500px] w-full relative pt-[25px]">
+                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664503/react-email-images/czdm06rsl3vxkpgnpwtj.png" alt="Big frame2" className="w-full object-cover" />
               </div>
-              <div className="max-w-[500px] w-full relative ">
-                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664719/react-email-images/scd9pfrfjarq5pmbsws7.png" alt="Big frame3" className="w-full object-cover pt-[25px]" />
+              <div className="max-w-[500px] w-full relative pt-[25px]">
+                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664719/react-email-images/scd9pfrfjarq5pmbsws7.png" alt="Big frame3" className="w-full object-cover" />
               </div>
             </Section>
           </Container>
