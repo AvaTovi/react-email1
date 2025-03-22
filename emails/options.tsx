b9778e0d28ae1f61f918f837a071c1a67c9bd966
Ava Tovi <avatovi45@icloud.com>
2025-03-22 09:57:28 -0700

    start

---
+++ emails/options.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Body, Container, Section, Html, Img, Text, Head, Tailwind } from "@react-email/components";
+
+export const options = () => {
+  return (
+    <Html>
+      <Tailwind>
+        <Head />
+        <Body>
+          <Container className="max-w-[600px] mx-auto">
+            <div className="w-full justify-center pt-[10px] items-center flex flex-row text-center">
+              <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742059963/react-email-images/bxve7zpqde3shxos0b0a.png" alt="icon1" className="mr-[5px] w-[30px]" />
+              <Text className="font-extrabold text-[17px] leading-[28px] text-[black]" style={{ fontFamily: "Gilroy, sans-serif" }}>
+                SkateApp
+              </Text>
+            </div>
+            <Section className="max-w-[500px] w-full flex justify-center mx-auto">
+              <div style={{ width: "500px", height: "1px", backgroundColor: "gray", marginTop: "25px" }} />
+              <Section className="max-w-[500px] w-full flex flex-row justify-start ">
+                <Text className="font-light text-[12px] ml-[10px] text-center text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
+                  Skatepark
+                </Text>
+              </Section>
+              <div style={{ width: "500px", height: "1px", backgroundColor: "gray", marginTop: "25px" }} />
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default options;
