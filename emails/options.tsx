3d1cb4678189706f955d425ad86bc107fd8ae2b7
Ava Tovi <avatovi45@icloud.com>
2025-03-22 10:32:45 -0700

    added images

--- emails/options.tsx
+++ emails/options.tsx
@@ -14,14 +14,35 @@ export const options = () => {
                 SkateApp
               </Text>
             </div>
-            <Section className="max-w-[500px] w-full flex justify-center mx-auto">
+            <Section className="max-w-[500px] w-full flex pt-[30px] justify-center mx-auto">
               <div style={{ width: "500px", height: "1px", backgroundColor: "gray" }} />
-              <Section className="max-w-[500px] w-full flex flex-row justify-start items-center ">
-                <Text className="font-light text-[13px] m-[20px] text-center text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
+              <div className="max-w-[500px] w-full flex flex-row justify-start items-center ">
+                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
                   Skatepark
                 </Text>
-              </Section>
+                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
+                  Street Spot
+                </Text>
+                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
+                  DIY
+                </Text>
+                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
+                  In progress
+                </Text>
+                <Text className="font-light text-[13px] m-[20px] text-center cursor-pointer text-[#2E2F30] " style={{ fontFamily: "Gilroy, sans-serif" }}>
+                  Dead Spot
+                </Text>
+              </div>
               <div style={{ width: "500px", height: "1px", backgroundColor: "gray" }} />
+              <div className="max-w-[500px] w-full relative ">
+                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664337/react-email-images/fyk6ktfzbk3efhovttz9.png" alt="Big frame1" className="w-full object-cover pt-[50px]" />
+              </div>
+              <div className="max-w-[500px] w-full relative ">
+                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664503/react-email-images/czdm06rsl3vxkpgnpwtj.png" alt="Big frame2" className="w-full object-cover pt-[25px]" />
+              </div>
+              <div className="max-w-[500px] w-full relative ">
+                <Img src="https://res.cloudinary.com/ds8slufsu/image/upload/v1742664719/react-email-images/scd9pfrfjarq5pmbsws7.png" alt="Big frame3" className="w-full object-cover pt-[25px]" />
+              </div>
             </Section>
           </Container>
         </Body>
